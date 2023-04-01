054169f9284678f264a1e2b157a1edb0e7e30f01
Nathan Getachew <nathangetachew95@gmail.com>
2023-04-01 17:51:15 +0300

    Initial Commit

---
+++ Firestore Chat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Firestore Chat
+//
+//  Created by Nathan Getachew on 4/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firestore Chat/Firestore_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firestore_ChatApp.swift
+//  Firestore Chat
+//
+//  Created by Nathan Getachew on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Firestore_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
